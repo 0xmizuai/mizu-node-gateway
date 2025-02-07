80e9e2631f735300ccd572e035de2dcbfe8ced22
dongshu2013 <dongs2011@gmail.com>
2025-02-06 21:16:36 -0800

    add maxOutput field for pool

--- src/sqls/pool.sql
+++ src/sqls/pool.sql
@@ -5,6 +5,7 @@ CREATE TABLE pools (
     owner TEXT NOT NULL,
     prices JSONB NOT NULL,
     contextLength INTEGER NOT NULL,
+    maxOutput INTEGER NOT NULL,
     status INTEGER NOT NULL DEFAULT 0,
     createdAt INTEGER,
     updatedAt INTEGER
@@ -14,8 +15,8 @@ CREATE INDEX idx_pools_name ON pools (name);
 CREATE INDEX idx_pools_owner ON pools (owner);
 CREATE INDEX idx_pools_model ON pools (model);
 
-INSERT INTO pools (name, model, owner, prices, contextLength, createdAt, updatedAt)
+INSERT INTO pools (name, model, owner, prices, contextLength, maxOutput, createdAt, updatedAt)
 VALUES 
-  ('mizu-deepseek-r1:1.5b', 'deepseek-r1:1.5b', '0x14301d0Ff94D5405aA4FE7B8AC1ac54231d1bD93', '{"input": 2, "output": 4}', 131072, 1738570131, 1738570131),
-  ('mizu-deepseek-r1:8b', 'deepseek-r1:8b', '0x14301d0Ff94D5405aA4FE7B8AC1ac54231d1bD93', '{"input": 5, "output": 10}', 131072, 1738570131, 1738570131),
-  ('mizu-deepseek-r1:14b', 'deepseek-r1:14b', '0x14301d0Ff94D5405aA4FE7B8AC1ac54231d1bD93', '{"input": 10, "output": 20}', 131072, 1738570131, 1738570131);
+  ('mizu-deepseek-r1:1.5b', 'deepseek-r1:1.5b', 'admin.mizu', '{"input": 2, "output": 4}', 131072, 4096, 1738570131, 1738570131),
+  ('mizu-deepseek-r1:8b', 'deepseek-r1:8b', 'admin.mizu', '{"input": 5, "output": 10}', 131072, 4096, 1738570131, 1738570131),
+  ('mizu-deepseek-r1:14b', 'deepseek-r1:14b', 'admin.mizu', '{"input": 10, "output": 20}', 131072, 4096, 1738570131, 1738570131);
