52767b63e12263e648dc93a638c9d62226a53d0f
dongshu2013 <dongs2011@gmail.com>
2025-02-12 00:47:36 -0800

    align endpoint name

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,7 @@ app.use(
 
 // Apply jwtAuthMiddleware only to specific routes
 app.use('/take_job', jwtOrApiKeyAuthMiddleware);
-app.use('/submit_job_result', jwtOrApiKeyAuthMiddleware);
+app.use('/submit_job_output', jwtOrApiKeyAuthMiddleware);
 app.use('/finish_job', jwtOrApiKeyAuthMiddleware);
 app.use('/publish_inference_jobs', jwtOrApiKeyAuthMiddleware);
 app.use('/job_results', jwtOrApiKeyAuthMiddleware);
@@ -99,7 +99,7 @@ openapi.registry.registerComponent('securitySchemes', 'BearerAuth', {
 
 // Define routes
 openapi.get('/take_job', TakeJob);
-openapi.post('/submit_job_result', SubmitJobOutput);
+openapi.post('/submit_job_output', SubmitJobOutput);
 openapi.post('/finish_job', FinishJob);
 openapi.post('/publish_inference_jobs', PublishInferenceJobs);
 openapi.get('/job_results', GetJobResults);
